a193bba08e32b2fbf6aaa21397eb343e7ce75234
Vladimir <vova206@gmail.com>
2020-04-20 23:55:03 +0300

    params in printing function
    moved reading BME values out of printValues()

--- screenManagement.ino
+++ screenManagement.ino
@@ -1,14 +1,27 @@
-void printValues() {
+void printValues(double t, double h, double p, double c) {
+
+  String tempStr = "T:" + (String)t + "C";
+  String humStr = "H:" + (String)h + "%";
+  String presStr = "P:" + (String)p + "mm";
+  String co2Str = "C:" + (String)c + "ppm";
 
   myOLED.clrScr();
-  myOLED.print("T:" + (String)bme.readTemperature() + "C", OLED_L, 1);
-  myOLED.print("H:" + (String)bme.readHumidity() + "%", OLED_L, 3);
-  myOLED.print("P:" + (String)(bme.readPressure() / 100.0F * 0.75006) + "mm", OLED_L, 5);
+  myOLED.print(tempStr, OLED_L, 1);
+  myOLED.print(humStr, OLED_L, 3);
+  myOLED.print(presStr, OLED_L, 5);
   //myOLED.print("A:" + (String)(bme.readAltitude(SEALEVELPRESSURE_HPA)) + "m", OLED_L, 7);
-  myOLED.print("C:" + (String)ppm + "ppm", OLED_L, 7);
+  myOLED.print(co2Str, OLED_L, 7);
+
+//  lcd.print(
+
 }
 
 void connectOLED() {
   myOLED.begin();
   myOLED.setFont(MediumFont);
+}
+
+void connectLCD() {
+  lcd.init();
+  lcd.backlight();
 }
